e55c3ad4a14957c6d0f6e13f49ab4871580e3f90
Philipp Allgeuer <philipp.allgeuer@uni-hamburg.de>
2022-11-21 14:01:22 +0100

    Add posture/animations/action queue

--- pepper_extra/scripts/pepper_api_interface.py
+++ pepper_extra/scripts/pepper_api_interface.py
@@ -5,6 +5,7 @@
 # Imports
 import Queue
 import rospy
+import std_srvs.srv
 import pepper_extra.srv
 from naoqi_driver.naoqi_node import NaoqiNode
 
@@ -24,10 +25,15 @@ class PepperAPIInterface(NaoqiNode):
 		self.leds_proxy = self.get_proxy("ALLeds")
 		self.tts_proxy = self.get_proxy("ALTextToSpeech")
 		self.speech_proxy = self.get_proxy("ALAnimatedSpeech")
+		self.anim_proxy = self.get_proxy("ALAnimationPlayer")
+		self.posture_proxy = self.get_proxy("ALRobotPosture")
 
 		self.srv_leds_set_rgb = rospy.Service("leds/set_rgb", pepper_extra.srv.LEDsSetRGB, self.handle_leds_set_rgb)
 		self.srv_speech_say = rospy.Service("speech/say", pepper_extra.srv.SpeechSay, self.handle_speech_say)
 		self.srv_speech_say_anim = rospy.Service("speech/say_anim", pepper_extra.srv.SpeechSay, self.handle_speech_say_anim)
+		self.srv_anim_play = rospy.Service("anim/play", pepper_extra.srv.AnimPlay, self.handle_anim_play)
+		self.srv_pose_set_posture = rospy.Service("pose/set_posture", pepper_extra.srv.SetPosture, self.handle_pose_set_posture)
+		self.srv_pose_home = rospy.Service("pose/home", std_srvs.srv.Empty, self.handle_pose_home)
 
 		self.action_queue = Queue.Queue()
 
@@ -39,7 +45,11 @@ class PepperAPIInterface(NaoqiNode):
 			rate.sleep()
 
 	def perform_actions(self):
-		pass  # TODO: Perform sequentially any actions available in the queue (.say() does not have _async)
+		try:
+			action = self.action_queue.get_nowait()
+			action[0](*action[1:])
+		except Queue.Empty:
+			pass
 
 	def handle_leds_set_rgb(self, msg):
 		try:
@@ -57,18 +67,7 @@ class PepperAPIInterface(NaoqiNode):
 			if msg.wait:
 				self.perform_speech_say(msg)
 			else:
-				raise NotImplementedError  # TODO: Use self.action_queue...
-			return pepper_extra.srv.SpeechSayResponse()
-		except RuntimeError as e:
-			rospy.logerr("Exception caught:\n%s", e)
-			return None
-
-	def handle_speech_say_anim(self, msg):
-		try:
-			if msg.wait:
-				self.perform_speech_say_anim(msg)
-			else:
-				raise NotImplementedError  # TODO: Use self.action_queue...
+				self.action_queue.put_nowait((self.perform_speech_say, msg))
 			return pepper_extra.srv.SpeechSayResponse()
 		except RuntimeError as e:
 			rospy.logerr("Exception caught:\n%s", e)
@@ -77,8 +76,55 @@ class PepperAPIInterface(NaoqiNode):
 	def perform_speech_say(self, msg):
 		self.tts_proxy.say(msg.text)
 
+	def handle_speech_say_anim(self, msg):
+		try:
+			if msg.wait:
+				self.perform_speech_say_anim(msg)
+			else:
+				self.action_queue.put_nowait((self.perform_speech_say_anim, msg))
+			return pepper_extra.srv.SpeechSayResponse()
+		except RuntimeError as e:
+			rospy.logerr("Exception caught:\n%s", e)
+			return None
+
 	def perform_speech_say_anim(self, msg):
 		self.speech_proxy.say(msg.text)
+
+	def handle_anim_play(self, msg):
+		try:
+			if msg.wait:
+				self.perform_anim_play(msg)
+			else:
+				self.action_queue.put_nowait((self.perform_anim_play, msg))
+			return pepper_extra.srv.AnimPlayResponse()
+		except RuntimeError as e:
+			rospy.logerr("Exception caught:\n%s", e)
+			return None
+
+	def perform_anim_play(self, msg):
+		self.anim_proxy.run(msg.anim_path)
+
+	def handle_pose_set_posture(self, msg):
+		try:
+			if msg.wait:
+				self.perform_pose_set_posture(msg.posture, msg.speed)
+			else:
+				self.action_queue.put_nowait((self.perform_pose_set_posture, msg.posture, msg.speed))
+			return pepper_extra.srv.SetPostureResponse()
+		except RuntimeError as e:
+			rospy.logerr("Exception caught:\n%s", e)
+			return None
+
+	def perform_pose_set_posture(self, posture, speed):
+		self.posture_proxy.goToPosture(posture, speed)
+
+	def handle_pose_home(self, msg):
+		try:
+			self.action_queue.put_nowait((self.perform_pose_set_posture, 'Stand', 0.3))
+			return std_srvs.srv.EmptyResponse()
+		except RuntimeError as e:
+			rospy.logerr("Exception caught:\n%s", e)
+			return None
 
 # Run main function
 if __name__ == "__main__":
